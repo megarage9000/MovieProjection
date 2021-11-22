df419e006edde3bfc325738e2ba20befb2ff5948
Quince Bielka <qbielka@sfu.ca>
2021-11-22 15:35:41 -0800

    Adds an additional question

--- AdultQuestion.py
+++ AdultQuestion.py
@@ -7,60 +7,38 @@ from sklearn.ensemble import RandomForestClassifier, StackingClassifier
 
 
 def is_adult_by_rating(data):
-    X = data[['averageRating']].values / 255
+    X = data[['averageRating']].values
     y = data['isAdult'].values
-    X_train, X_valid, y_train, y_valid = train_test_split(X, y)
-
-    gauss_model = GaussianNB()
-    gauss_model.fit(X_train, y_train)
-    print("Gauss:", gauss_model.score(X_valid, y_valid))
-
-    # calculated based on running the model on everything between 1 and 300
-    neighbours_model = KNeighborsClassifier(56)
-    neighbours_model.fit(X_train, y_train)
-    print("K Neighbours:", neighbours_model.score(X_valid, y_valid))
-
-    random_forest_model = RandomForestClassifier(n_estimators=100)
-    random_forest_model.fit(X_train, y_train)
-    print("Random Forest:", random_forest_model.score(X_valid, y_valid))
-
-    artificial_intel = MLPClassifier(hidden_layer_sizes=(20))
-    artificial_intel.fit(X_train, y_train)
-    print("Machine Learning:", artificial_intel.score(X_valid, y_valid))
-
-    sc =StackingClassifier(
-        estimators=[
-            ("gauss", gauss_model),
-            ("kn", neighbours_model),
-            ("rf", random_forest_model),
-            ("ai", artificial_intel)]
-    )
-    sc.fit(X_train, y_train)
-    print("StackingClassifier:", sc.score(X_valid, y_valid))
-
+    multi_classifiers(X, y)
 
 def is_adult_by_num_rated(data):
-    X = data[['numVotes']].values / 255
+    X = data[['numVotes']].values
     y = data['isAdult'].values
-    X_train, X_valid, y_train, y_valid = train_test_split(X, y)
+    multi_classifiers(X, y)
 
+
+def is_adult_by_num_rated_and_rating(data):
+
+    X = data.filter(['numVotes','averageRating']).to_numpy()
+    y = data['isAdult'].values
+    multi_classifiers(X, y)
+
+
+def multi_classifiers(X, y):
+    X_train, X_valid, y_train, y_valid = train_test_split(X, y)
     gauss_model = GaussianNB()
     gauss_model.fit(X_train, y_train)
     print("Gauss:", gauss_model.score(X_valid, y_valid))
-
     # calculated based on running the model on everything between 1 and 300
     neighbours_model = KNeighborsClassifier(56)
     neighbours_model.fit(X_train, y_train)
     print("K Neighbours:", neighbours_model.score(X_valid, y_valid))
-
     random_forest_model = RandomForestClassifier(n_estimators=100)
     random_forest_model.fit(X_train, y_train)
     print("Random Forest:", random_forest_model.score(X_valid, y_valid))
-
     artificial_intel = MLPClassifier(hidden_layer_sizes=(20))
     artificial_intel.fit(X_train, y_train)
     print("Machine Learning:", artificial_intel.score(X_valid, y_valid))
-
     sc = StackingClassifier(
         estimators=[
             ("gauss", gauss_model),
@@ -98,10 +76,12 @@ def main():
     #   and they just started guessing always true for that question
     equalized_rows = equalize_adult_non_adult(combined_adult_ratings)
 
-    print("By Rating")
+    print("Asks various classifiers to decide if a movie is adult based on its rating")
     is_adult_by_rating(equalized_rows)
-    print("By Num Rated")
+    print("\nAsks various classifiers to decide if a movie is adult based on how many people rated it")
     is_adult_by_num_rated(equalized_rows)
+    print("\nAsks various classifiers to decide if a movie is adult based on how many people rated it and its rating")
+    is_adult_by_num_rated_and_rating(equalized_rows)
 
 
 
