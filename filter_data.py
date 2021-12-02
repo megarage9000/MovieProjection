a3f07d7b99d78675dd76ec85d6584dd415033f8e
John Ordoyo <gtom_1984@hotmail.ca>
2021-12-01 20:31:46 -0800

    movieProjection: Create new data for additional ETL
    - Need to make another file to further filter data

--- filter_data.py
+++ filter_data.py
@@ -8,6 +8,7 @@ def main():
     for file in files:
         filterbyTConst(t_consts, file, "tconst")
     filterbyTConst(t_consts, "title.akas.tsv.gz", "titleId")
+    filterNames()
 
 
 def getTconstsAndSave(file_name):
@@ -21,5 +22,12 @@ def filterbyTConst(tconst_arr, file_name, col_name):
     frame = pd.read_csv(file_name, sep='\t', na_values='\\N')
     frame = frame[frame[col_name].isin(tconst_arr)]
     frame.to_csv("filtered_"+file_name+".csv")
+
+def filterNames():
+    names = pd.read_csv('name.basics.tsv.gz', sep='\t', na_values='\\N')
+    names['professions'] = names['primaryProfession'].str.split(',')
+    names = names[['nconst','primaryName', 'professions']]
+    names = names.explode('professions')
+    names.to_csv('filtered_name.tsv.gz.csv')
 
 main()
--- movieTrend.py
+++ movieTrend.py
@@ -4,7 +4,14 @@ from sklearn import linear_model.LinearRegression
 
 
 
+def load_data(movie_data, movie_principals, names):
+    basics = movie_data[['tconst', 'primaryTitle', 'startYear', 'runtimeMinutes', 'genres', 'isAdult']]
+
 def main():
+    movie_data = pd.read_csv('filtered_title.basics.tsv.gz.csv')
+    movie_principals = pd.read_csv('filtered_title.principals.tsv.gz.csv')
+    names = pd.read_csv('filtered_name.tsv.gz.csv')
+    load_data(movie_data, movie_principals, names)
     print('hello world')
     
 
