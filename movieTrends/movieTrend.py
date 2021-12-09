8a7956934db601db6bc1b0c2909959f0364c9d88
John <gtom_1984@hotmail.ca>
2021-12-09 15:13:38 -0800

    movieProjection: Need to do feature engineering
    - take a look at the following attributes [startyear, runtimeMinutes]
      and see if they are valid enough for machine learning
    - consider scaling down the number of categories in genres / languages

--- movieTrends/movieTrend.py
+++ movieTrends/movieTrend.py
@@ -8,9 +8,10 @@ from sklearn.preprocessing import StandardScaler
 
 # Most code borrowed from "Hands-on Machine Learning with Scikit-learn, Keras, and Tensorflow 2nd Edition"
 
-BATCH_SIZE = 200
-CATEGORIES = ['genres', 'startYear']
+CATEGORIES = ['genres', 'startYear', 'language']
 NUM_CAT = len(CATEGORIES)
+NUM_EPOCHS = 20
+LEARNING_RATE = 0.01
 
 def getTrainModel(movie_data):
     X_data = movie_data[movie_data.columns.difference(['averageRating'])]
@@ -32,8 +33,12 @@ def getTrainModel(movie_data):
     X_valid_num = scaler.transform(X_valid_num)
     X_test_num = scaler.transform(X_test_num)
 
-    result = model.fit((X_train_num, np.split(X_train_cat, NUM_CAT, axis=1)), y_train, epochs=20,
-                       validation_data=((X_valid_num, np.split(X_valid_cat, NUM_CAT, axis=1)), y_valid))
+    # Get learning rate scheduler
+    exp_decay_fn = exp_decay(lr0=LEARNING_RATE, s=NUM_EPOCHS)
+    lr_scheduler = keras.callbacks.LearningRateScheduler(exp_decay_fn)
+
+    result = model.fit((X_train_num, np.split(X_train_cat, NUM_CAT, axis=1)), y_train, epochs=NUM_EPOCHS,
+                       validation_data=((X_valid_num, np.split(X_valid_cat, NUM_CAT, axis=1)), y_valid), callbacks=[lr_scheduler])
     test = model.evaluate((X_test_num, np.split(X_test_cat, NUM_CAT, axis=1)), y_test)
     print(test)
 
@@ -54,8 +59,6 @@ def createModel(X_data):
     # Get numerical inputs
     num_numerical = len(X_data.columns) - NUM_CAT
     numerical_inputs = keras.layers.Input(shape=[num_numerical], dtype=tf.float64)
-    layer_num_1 = keras.layers.Dense(BATCH_SIZE, activation="relu")(numerical_inputs)
-    layer_num_2 = keras.layers.Dense(BATCH_SIZE, activation="sigmoid")(layer_num_1)
 
     # Get categorical inputs (genres, startYear, isAdult)
     categorical_inputs_names = CATEGORIES
@@ -68,18 +71,28 @@ def createModel(X_data):
     embedded_layers = keras.layers.concatenate(categorical_embedded_layers)
 
     # Apply layers on embedded
-    embed_sigmoid_layer = keras.layers.Dense(BATCH_SIZE, activation="relu")(embedded_layers)
-    flattened_layer = keras.layers.Flatten()(embed_sigmoid_layer)
+    flattened_layer = keras.layers.Flatten()(embedded_layers)
 
     # Concatenate the inputs
-    all_inputs = keras.layers.concatenate([layer_num_2, flattened_layer])
+    all_inputs = keras.layers.concatenate([numerical_inputs, flattened_layer])
+    hidden_layers = applyLayers(all_inputs)
 
     # Dense inputs to one
-    rating_output = keras.layers.Dense(1)(all_inputs)
+    rating_output = keras.layers.Dense(1)(hidden_layers)
+
     movie_rating_model = keras.models.Model(inputs=[numerical_inputs, categorical_inputs], outputs=[rating_output])
-    movie_rating_model.compile(loss=keras.losses.MeanAbsoluteError(), optimizer=keras.optimizers.Adam(), metrics=['accuracy'])
+    movie_rating_model.compile(loss=keras.losses.MeanSquaredError(),
+                               optimizer=keras.optimizers.Nadam(learning_rate=LEARNING_RATE),
+                               metrics=['accuracy'])
 
     return movie_rating_model
+
+
+# From Hands-on-ML, exponential scheduling
+def exp_decay(lr0, s):
+    def exp_decay_fn(epoch):
+        return lr0 * 0.1**(epoch / s)
+    return exp_decay_fn
 
 
 def setupCategorialLayer(data, name, number_oov):
@@ -96,28 +109,46 @@ def setupCategorialLayer(data, name, number_oov):
     table = tf.lookup.StaticVocabularyTable(table_init, number_oov)
 
     # Create input layer for the category
-    categories = keras.layers.Input(shape=[1], dtype=tf.string)
+    categories = keras.layers.Input(shape=[], dtype=tf.string)
 
     # Create embedded layer for the categorical value
     category_indices = keras.layers.Lambda(lambda category: table.lookup(category))(categories)
     embedded_layer = keras.layers. \
         Embedding(input_dim=num_category_values + number_oov, output_dim=int(math.sqrt(num_category_values))) \
         (category_indices)
-    relu_layer = keras.layers.Dense(BATCH_SIZE, activation='sigmoid')(embedded_layer)
     # Return the embedded layer along with the input layer
-    return relu_layer, categories
+    return embedded_layer, categories
 
+
+LAYER_SIZE = 100
+
+def applyLayers(layers):
+    batch = keras.layers.BatchNormalization()(layers)
+    selu_layer = keras.layers.Dense(300, activation='selu', kernel_initializer="lecun_normal", use_bias=False)(batch)
+    batch_2 = keras.layers.BatchNormalization()(selu_layer)
+    elu_layer = keras.layers.Dense(100, activation='elu', kernel_initializer='he_normal', use_bias=False)(batch_2)
+    batch_3 = keras.layers.BatchNormalization()(elu_layer)
+    final_layer = keras.layers.Dense(50)(batch_3)
+    return final_layer
 
 def main():
+    movie_data = processData()
+    print(movie_data.shape)
+    resulting_model = getTrainModel(movie_data)
+
+
+def processData():
     movie_data = pd.read_csv('../filtered_title.basics.tsv.gz.csv')
     ratings = pd.read_csv('../filtered_title.ratings.tsv.gz.csv')
+    akas = pd.read_csv('../filtered_title.akas.tsv.gz.csv')
+    akas['tconst'] = akas['titleId']
     movie_data = movie_data.merge(ratings, on=['tconst'])
+    movie_data = movie_data.merge(akas, on=['tconst'])
 
     # Start with basic data first
-    movie_data = movie_data[['startYear', 'runtimeMinutes', 'isAdult', 'genres', 'averageRating']]
-    movie_data = movie_data.fillna(0)
-    print(movie_data.shape)
-    # resulting_model = getTrainModel(movie_data)
-
+    movie_data = movie_data[
+        ['startYear', 'runtimeMinutes', 'isAdult', 'genres', 'language', 'isOriginalTitle', 'averageRating']]
+    movie_data = movie_data.dropna(subset=['startYear', 'genres', 'language', 'runtimeMinutes'])
+    return movie_data
 
 main()
